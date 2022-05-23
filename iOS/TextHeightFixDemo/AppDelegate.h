4a04c49e27501444d303cd1ff22e3b1b663038e1
Simo_1201 <X18268210923@163.com>
2022-05-23 16:38:53 +0800

    Add files via upload

---
+++ iOS/TextHeightFixDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TextHeightFixDemo
+//
+//  Created by Ang Li on 2022/5/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOS/TextHeightFixDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TextHeightFixDemo
+//
+//  Created by Ang Li on 2022/5/23.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
